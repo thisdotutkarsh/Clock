a2b81710782b5b4fef65231b8da6c530e07191f7
thisdotutkarsh <srivastava_utkarsh@outlook.com>
2020-12-28 23:43:44 +0530

    Fixed Issue Related to Overlapping of Clock's Hour, Minute & Second Hands. Added JavaScript Function to Set & Update Clock's Hour, Minute & Seconds Hands Rotation Angles Per Second

--- main.js
+++ main.js
@@ -1,3 +1,27 @@
 window.onload = (event) => {
-    console.log('Ready.')
+    console.log('Ready.');
+    setTime();
+}
+
+setInterval(setTime, 1000);
+
+const hourHand = document.querySelector('[data-hourHand]');
+const minuteHand = document.querySelector('[data-minuteHand]');
+const secondHand = document.querySelector('[data-secondHand]');
+
+function setTime() {
+    const dateToday = new Date();
+
+    const seconds = dateToday.getSeconds() / 60;
+    const minutes = (seconds + dateToday.getMinutes()) / 60;
+    const hours = (minutes + dateToday.getHours()) / 12;
+
+    setRotationDegrees(secondHand, seconds);
+    setRotationDegrees(minuteHand, minutes);
+    setRotationDegrees(hourHand, hours);
+
+}
+
+function setRotationDegrees(clockHand, rotationDegree) {
+    clockHand.style.setProperty('--clock-hands-rotation', rotationDegree * 360)
 }
